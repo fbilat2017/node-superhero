84d953c35ecda751a072b82929f9a2a7396fe12e
fbilat2017 <fbilat2017@gmail.com>
2026-02-05 10:45:19 -0300

    primera comunicacion con la api, obteniendo personaje

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+const axios = require('axios');
+
+const API_KEY = '572a3679ce5dde457715cf887cac935b'; // token de https://superheroapi.com
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/hero/:id', async (req, res) => {
+    try {
+        const url = `https://superheroapi.com/api/${API_KEY}/${req.params.id}`;
+        const response = await axios.get(url);
+        // Renderizamos el archivo 'perfil.pug' pasando los datos del héroe
+        res.render('hero/perfil', { hero: response.data });
+    } catch (error) {
+        res.status(500).send("Error al obtener datos");
+    }
+});
+
 module.exports = router;
+
+
+
+
